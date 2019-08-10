8839238e02c16c6b7ae25d0556c0b1e6d6e1a51b
suika <53206264+suikastar@users.noreply.github.com>
2019-08-10 23:27:40 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,12 +1,16 @@
 addEventListener('fetch', event => {
-  event.respondWith(proxy(event));
+    event.respondWith(proxy(event));
 });
 
 async function proxy(event) {
-  let url = new URL(event.request.url);
-  const domain = url.searchParams.get('domain');
-  url.searchParams.delete('domain')
-  url.hostname = domain;
-  event.request.headers.Host = domain;
-  return fetch(new Request(url, event.request));
+    let url = new URL(event.request.url);
+    let pathname = url.pathname;
+    let sub_pathname_arr = pathname.split("/");
+    const domain = sub_pathname_arr[1];
+    sub_pathname_arr.splice(0, 2);
+    pathname = '/' + sub_pathname_arr.join('/');
+    url.pathname = pathname;
+    url.hostname = domain;
+    event.request.headers.Host = domain;
+    return fetch(new Request(url, event.request));
 }
